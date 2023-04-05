be628151cb3102ea2e1795a9a22ba97d89507d5a
Phönix 64 <piosbenni@gmail.com>
2023-04-05 11:43:44 +0200

    ahh more

--- heat.c
+++ heat.c
@@ -213,6 +213,17 @@
 #define FLOOR 0         /* set to 1 to limit wave amplitude to VMAX */
 #define VMAX 10.0       /* max value of wave amplitude */
 
+/* added missing defines*/
+#define DRAW_BILLIARD 0
+#define DT 0.00000025
+#define VISCOSITY 2.0
+#define SPEED 0.0       /* speed of drift to the right */
+#define F_GRADIENT 0
+#define F_INTENSITY 0
+#define FIELD_REP 0
+#define DRAW_FIELD_LINES 0
+#define FIELD_LINE_FACTOR 0
+#define N_FIELD_LINES 0
 
 
 #include "global_pdes.c"
